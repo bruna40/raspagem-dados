3de7425a826e8425bb1a47505983cc567dde066f
Bruna Santiago <brunasanthiago@outlook.com>
2023-03-02 14:30:30 -0300

    adicionando o readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
  # Raspagem Dados da URL
+
+
+O projeto em questão foi desenvolvido em Python com o objetivo de extrair informações de uma URL utilizando conceitos de orientação a objetos. O programa recebe uma URL como entrada e extrair informações como o título da página, links e tabelas presentes no site.
+
+Com uma estrutura de classes bem definida, o programa permite uma fácil manutenção e extensão para novas funcionalidades. Além disso, o projeto foi desenvolvido como parte do curso de Python da Alura, o que garante uma abordagem prática e alinhada com as melhores práticas de desenvolvimento de software.
+
+Caso você precise extrair informações de uma URL de forma automatizada, este projeto pode ser uma boa referência para iniciar seus próprios projetos.
---
+++ extrator_url.py
@@ -0,0 +1,38 @@
+class ExtratorURL:
+    def __init__(self, url):
+        self.url = self.sanitiza_url(url)
+        self.valida_url()
+
+    def sanitiza_url(self, url):
+        if type(url) == str:
+            return url.strip()
+        else:
+            return ''
+
+    def valida_url(self):
+        if not bool(self.url):
+            raise ValueError('A URL está vazia')
+
+    def get_url_base(self):
+        indice_interrogacao = self.url.find('?')
+        return self.url[:indice_interrogacao]
+
+    def get_url_parametros(self):
+        indice_interrogacao = self.url.find('?')
+        return self.url[indice_interrogacao+1:]
+
+    def get_valor_parametro(self, parametro_busca):
+        indice_parametro = self.get_url_parametros().find(parametro_busca)
+        indice_valor = indice_parametro + len(parametro_busca) + 1
+        indice_e_comercial = self.get_url_parametros().find('&', indice_valor)
+
+        if indice_e_comercial == -1:
+            return self.get_url_parametros()[indice_valor:]
+        else:
+            return self.get_url_parametros()[indice_valor:indice_e_comercial]
+
+
+valor = ExtratorURL(None)
+quantidade = valor.get_valor_parametro('quantidade')
+
+print(quantidade)
